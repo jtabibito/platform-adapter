83cf759a50f0d657a659bc165f26a5c4091f58be
jtabibito <jtabibito@qq.com>
2025-04-15 20:07:40 +0800

    Revert "feat: add tiktok polyfill"
    This reverts commit f71d3cc63e021eb95871d9c6f80cb1f2f29bc01c.

--- src/platforms/tiktok/minigame/polyfill/WindowProperties.ts
+++
@@ -1,7 +0,0 @@
-const { screenWidth, screenHeight, devicePixelRatio } = tt.getSystemInfoSync();
-
-export const innerWidth = screenWidth;
-export const innerHeight = screenHeight;
-export { devicePixelRatio };
-
-export { default as performance } from './Performance';
