b97590aa7a759aa9e1e94566e8881bf7c82c84ac
jtabibito <jtabibito@qq.com>
2025-10-24 17:23:44 +0800

    refactor: remove redundant Chinese comment from escapeGlob function
    - Remove the Chinese comment explaining regex replacement
    - Keep the code self-documenting with clear function name and JSDoc
    - Maintain the same functionality while improving code cleanliness

--- builder/utils/Utils.ts
+++ builder/utils/Utils.ts
@@ -64,7 +64,6 @@ function createResolveMatcher(id: string) {
  * @returns {string} The escaped string.
  */
 function escapeGlob(string: string) {
-  // 使用正则表达式匹配所有特殊字符并在前面加上反斜杠转义
   return string.replace(/[\\?*\[\]{}()^!]/g, '\\$&');
 }
 
